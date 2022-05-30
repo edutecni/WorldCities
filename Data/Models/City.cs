06a17495535a533a977e0e112c8c818ee5912242
Eduardo Alves Canuto <edutecni@gmail.com>
2022-05-30 08:52:04 -0300

    Install Entitys Libs, and add Models

---
+++ Data/Models/City.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        #region Constructor
+        public City()
+        {
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Name_ASCII { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+        public int CountryId { get; set; }
+
+        #endregion
+    }
+}
---
+++ Data/Models/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class Country
+    {
+        #region Constructor
+        public Country()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        ///<summary>
+        /// The unique id and primary key for this Country
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+
+        #endregion
+
+    }
+}
