3024b83b00342a5b8a887b33f305d0f262e38195
Eduardo Alves Canuto <edutecni@gmail.com>
2022-06-04 07:31:55 -0300

    Implementando Contexto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions  options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<City>()
+                .Property(x => x.Id).IsRequired();
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Country>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Country>()
+                .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+                .HasOne(x => x.Country)
+                .WithMany(y => y.Cities)
+                .HasForeignKey(x => x.CountryId);
+
+            // add the EntityTypeConfiguration classes
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly
+                );
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        
+    }
+}
