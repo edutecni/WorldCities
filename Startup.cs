b84a3124e5a66ad957dc920f740c47fbff72b0b1
Eduardo Alves Canuto <edutecni@gmail.com>
2022-06-09 07:29:01 -0300

    Identação dos Jsons

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,14 @@ namespace WorldCities
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddJsonOptions(options => {
+                    // Set this option to True to ident the Json output  
+                    options.JsonSerializerOptions.WriteIndented = true;
+                    // set his option to Null to use PascalCase instead of
+                    //  camelCase (default)
+                    //options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
