5cb1f66cac0ae76b5c2a73ef3bddf42389b601b3
Eduardo Alves Canuto <edutecni@gmail.com>
2022-09-26 06:17:03 -0300

    Ajustes

--- ClientApp/src/app/cities/city-edit.component.ts
+++ ClientApp/src/app/cities/city-edit.component.ts
@@ -3,18 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { City } from './city';
+
 @Component({
   selector: 'app-city-edit',
   templateUrl: './city-edit.component.html',
   styleUrls: ['./city-edit.component.css']
 })
+
 export class CityEditComponent implements OnInit {
-  // the view title
+  // O título da View
   title: string;
+
   // the form model
   form: FormGroup;
-  // the city object to edit
+
+  // O objeto Cidade para a edição
   city: City;
+
+  // Será NULL quando for adicionado uma nova Cidade
+  // e não será nulo  quando estiver editando uma Cidade existente
+ // id?: number;
+
   constructor(
     private activatedRout: ActivatedRoute,
     private router: Router,
@@ -28,15 +37,18 @@ export class CityEditComponent implements OnInit {
     });
     this.loadData();
   }
+
   loadData() {
-    // retrieve the ID from the 'id' parameter
+    // Retorno do parâmetro ID
     var id = +this.activatedRout.snapshot.paramMap.get('id');
-    // fetch the city from the server
+
+    // Localzando a cidade no servidor
     var url = this.baseUrl + "api/Cities/" + id;
     this.http.get<City>(url).subscribe(result => {
       this.city = result;
       this.title = "Edit - " + this.city.name;
-      // update the form with the city value
+
+      // Atualizando o form pela cidade como valor
       this.form.patchValue(this.city);
     }, error => console.error(error));
   }
@@ -49,7 +61,8 @@ export class CityEditComponent implements OnInit {
     this.http.put<City>(url, city)
       .subscribe(result => {
         console.log("City " + city.id + " has been updated.");
-        // go back to cities view
+
+        // Voltando para a View Cidade
         this.router.navigate(['/cities']);
       }, error => console.error(error));
   }
